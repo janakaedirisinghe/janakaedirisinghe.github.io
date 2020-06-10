0c82190872e60e30858fe4d76a5b58e3e464feb1
Janaka Edirisinghe <janakapradeepedirisinghe@gmail.com>
2020-06-10 15:00:19 +0530

    updated

---
+++ js/script.js
@@ -0,0 +1,25 @@
+
+// function to set a given theme/color-scheme
+function setTheme(themeName) {
+    localStorage.setItem('theme', themeName);
+    document.documentElement.className = themeName;
+}
+
+// function to toggle between light and dark theme
+function toggleTheme() {
+    console.log('hello');
+    if (localStorage.getItem('theme') === 'theme-dark') {
+        setTheme('theme-light');
+    } else {
+        setTheme('theme-dark');
+    }
+}
+
+// Immediately invoked function to set the theme on initial load
+(function () {
+    if (localStorage.getItem('theme') === 'theme-dark') {
+        setTheme('theme-dark');
+    } else {
+        setTheme('theme-light');
+    }
+})();
