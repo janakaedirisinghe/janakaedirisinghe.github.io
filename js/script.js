aaaf45046500ff204f395b9413c084a42ac4b396
janakaedirisinghe <janakapradeepedirisinghe@gmail.com>
2024-03-03 20:16:12 +0530

    dark/light mode added

--- js/script.js
+++ js/script.js
@@ -25,19 +25,47 @@ function toggleTheme() {
 })();
 
 fetch('https://api.github.com/users/janakaedirisinghe')
-  .then(response => response.json())
-  .then(data => {
-    //   console.log(data);
-      document.getElementById("public_gists").innerHTML = data.public_gists+' Gits';
-      document.getElementById("followers").innerHTML = data.followers+ ' Followers';
-      document.getElementById("public_repos").innerHTML = data.public_repos+ ' Public Repos';
-      document.getElementById("github_avatar").src = data.avatar_url;
-  });
-  fetch('  https://api.stackexchange.com/2.2/users/10215448?order=desc&sort=reputation&site=stackoverflow')
-  .then(response => response.json())
-  .then(data => {
-    //   console.log(data.items[0]);
-      document.getElementById("reputation_change_year").innerHTML = data.items[0].reputation_change_year+ ' This year';
+    .then(response => response.json())
+    .then(data => {
+        //   console.log(data);
+        document.getElementById("public_gists").innerHTML = data.public_gists + ' Gits';
+        document.getElementById("followers").innerHTML = data.followers + ' Followers';
+        document.getElementById("public_repos").innerHTML = data.public_repos + ' Public Repos';
+        document.getElementById("github_avatar").src = data.avatar_url;
+    });
+fetch('  https://api.stackexchange.com/2.2/users/10215448?order=desc&sort=reputation&site=stackoverflow')
+    .then(response => response.json())
+    .then(data => {
+        //   console.log(data.items[0]);
+        document.getElementById("reputation_change_year").innerHTML = data.items[0].reputation_change_year + ' This year';
 
-      document.getElementById("reputation").innerHTML = data.items[0].reputation+ ' Reputation';
-  });
+        document.getElementById("reputation").innerHTML = data.items[0].reputation + ' Reputation';
+    });
+
+
+
+document.addEventListener('DOMContentLoaded', function () {
+    const themeSwitcher = document.getElementById('theme-switcher');
+
+    const savedTheme = localStorage.getItem('theme');
+    if (savedTheme === 'theme-dark') {
+        setTheme('theme-dark');
+        themeSwitcher.checked = true;
+    } else {
+        setTheme('theme-light');
+        themeSwitcher.checked = false;
+    }
+
+    themeSwitcher.addEventListener('change', function () {
+        if (themeSwitcher.checked) {
+            setTheme('theme-dark');
+        } else {
+            setTheme('theme-light');
+        }
+        localStorage.setItem('theme', this.checked ? 'theme-dark' : 'theme-light');
+    });
+});
+
+
+
+Resources
